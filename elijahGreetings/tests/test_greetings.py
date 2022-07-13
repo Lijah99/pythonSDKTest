c5f3aaeff223f224c210adf17da246006f113bfe
GARMIN\flinderselija <elijah.flinders@garmin.com>
2022-07-13 16:11:37 -0600

    testing good lint
    Change-Id: I6d12a031dda7b3e8c338e15fa5aed377dc3e8af1

--- elijahGreetings/tests/test_greetings.py
+++ elijahGreetings/tests/test_greetings.py
@@ -1,10 +1,12 @@
 import math
 from elijahGreetings.src import greetings_function as greet
 
+
 def test_sqrt():
-   num = 25
-   assert math.sqrt(num) == 5
+    num = 25
+    assert math.sqrt(num) == 5
+
 
 def test_say_hello():
-  output = greet.say_hello('eli')
-  assert output == 'Hello eli!'
+    output = greet.say_hello("eli")
+    assert output == "Hello eli!"
