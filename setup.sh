03782d6bdd094cb5a7c149b07b0a7fb2a47f25a6
GARMIN\flinderselija <elijah.flinders@garmin.com>
2022-07-13 13:12:54 -0600

    add specified python version to setup script
    Change-Id: I30ee2a2a304a108ff5ab4266e18914162da818dd

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env py
 
 #install virtualenv and create the environment
-py -3 -m pip install virtualenv
-py -3 -m venv venv
+py -3.9 -m pip install virtualenv
+py -3.9 -m venv venv
 
 #activate it
 source venv/Scripts/activate
