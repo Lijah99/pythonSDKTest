7c6b22c658f1d8e2522794e77969095ed6c0d350
GARMIN\flinderselija <elijah.flinders@garmin.com>
2022-07-13 13:00:40 -0600

    Add comments to the script
    Change-Id: I8fa9fe2efc83dbe10b9f854f5801ff5b4621aa97

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env py
+
+#install virtualenv and create the environment
 py -3 -m pip install virtualenv
 py -3 -m venv venv
 
+#activate it
 source venv/Scripts/activate
 
+#install all necessary packages
 pip install -r requirements.txt
